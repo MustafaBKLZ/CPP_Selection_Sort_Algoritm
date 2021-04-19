4c9d3efdbbc53cbc16cbe0bb476958eec1a1e83a
Mustafa BÜKÜLMEZ <mustafabukulmez3446@gmail.com>
2021-04-19 14:13:18 +0300

    CPP_Selection_Sort_Algoritm

---
+++ CPP_Selection_Sort_Algoritm/CPP_Selection_Sort_Algoritm/Main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>  
+#include <ctime>  
+#include <cstdlib>  
+using namespace std;
+
+
+void printArray(int dizii[], int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << dizii[i] << " ";
+	cout << endl;
+}
+
+int main() {
+	int Sayilar[] = { 5, 6, 8, 1, 88, 99, 78, 45, 36, 25, 12, 9, 7 };
+	int  enkucuk, enbuyuk, yedek;
+	int n = sizeof(Sayilar) / sizeof(Sayilar[0]);
+
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		enkucuk = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (Sayilar[j] < Sayilar[enkucuk])
+			{
+				enkucuk = j;
+			}
+		}
+		if (enkucuk != i)
+		{
+			yedek = Sayilar[i];
+			Sayilar[i] = Sayilar[enkucuk];
+			Sayilar[enkucuk] = yedek;
+		}
+		printArray(Sayilar, n); // aþama aþama gösterir.
+	}
+	//printArray(Sayilar, n); // sadece sonucu gosterir.
+}
+
